851cda08782163e7da75e7935c9c6a3f6f8b0a03
Hari <manjaka.rajaonson@gmail.com>
2022-07-07 18:45:23 +0300

    add API documentation

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -5,9 +5,48 @@
 const port = process.env.PORT || 3001;
 const databaseUrl = process.env.DATABASE_HOST;
 const databaseName = process.env.DATABASE_NAME;
+const documentation = {
+    "/": {
+        type: "GET",
+        description: "Displays exhaustive list of all API routes and their parameters."
+    },
+    "account": {
+        type: "GET",
+        description: "Gets all account list"
+    },
+    "account/create": {
+        type: "POST",
+        description: "Creates new account",
+        parameters: {
+            "firstname": "String",
+            "lastname": "String",
+            "avatar": "String",
+            "email": "String, required",
+            "password": "String, required",
+            "app": "String, required"
+        }
+    },
+    "account/:id": [
+        {
+            type: "PUT",
+            description: "Updates account information",
+            parameters: {
+                "firstname": "String",
+                "lastname": "String",
+                "avatar": "String",
+                "password": "String"
+            }
+        },
+        {
+            type: "DELETE",
+            description: "Deletes account"
+        }
+    ]
+};
 
 module.exports = {
     port,
     databaseUrl,
-    databaseName
+    databaseName,
+    documentation
 };
--- src/routes/docRoute.js
+++ src/routes/docRoute.js
@@ -1,17 +1,13 @@
 const router = require("express").Router();
-const { answer } = require("../utils");
+const { documentation } = require("../helpers/const");
+const { success } = require("../utils");
 
 /*
     Route for API documentation
 */
 
 router.get("/", (req, res) => {
-    res.json(answer("HAuth API documentation", [
-        {
-            route: "/",
-            description: "Displays exhaustive list of all API routes and their parameters."
-        }
-    ], 1));
+    res.json(success("HAuth API documentation", documentation));
 });
 
 module.exports = { path: "/", router };
